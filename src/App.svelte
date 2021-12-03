ea8d9d85434969952f2859fa0a91e423e62523ab
Jesper Orb <github@jesperorb.com>
2021-12-03 07:58:24 +0100

    feat: open all links in new tab

--- src/App.svelte
+++ src/App.svelte
@@ -12,14 +12,23 @@
 <header>
   <h1>DateTimeFormat Explorer</h1>
   <p>
-    Examples on how a date is formatted with <a
+    Examples on how a date is formatted with
+    <a
       href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat"
-      >MDN: Intl.DateTimeFormat</a
+      target="_blank"
+      rel="noopener noreferrer">MDN: Intl.DateTimeFormat</a
     >. This is not a precise tool, it iterates every options property as is. If
     you have found a bug, file an issue or open a PR in the
-    <a href="https://github.com/jesperorb/datetimeformat-explorer"
-      >repo for this tool</a
-    >. Created by <a href="https://jesperorb.com">Jesper Orb</a>.
+    <a
+      href="https://github.com/jesperorb/datetimeformat-explorer"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      repo for this tool</a
+    >. Created by
+    <a href="https://jesperorb.com" target="_blank" rel="noopener noreferrer"
+      >Jesper Orb</a
+    >.
   </p>
 
   <div class="options">
