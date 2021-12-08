894d3689f67605e9074dd8f04163b0756667a5a4
Jesper Orb <github@jesperorb.com>
2021-12-08 19:40:41 +0100

    feat: add display on units to numberformat tab

--- src/components/tabs/NumberFormatTab.svelte
+++ src/components/tabs/NumberFormatTab.svelte
@@ -5,14 +5,23 @@
   import Highlight from "../Highlight.svelte";
 
   import { currenciesAsEntries } from "../../locale-data/currencies";
-  import { numberFormatOptionsCurrency } from "../../options/number-format-options";
+  import {
+    numberFormatOptionsCurrency,
+    numberFormatOptionsUnit,
+  } from "../../options/number-format-options";
   import type { OptionValues } from "../../types/option-values";
   import { copyToClipboard } from "../../utils/copyToClipboard";
+  import { unitsAsEntries } from "../../locale-data/units";
 
   export let selectedLocale: string;
 
+  let selectedFormatter: "currency" | "unit" = "unit";
   let selectedCurrency = "EUR";
+  let selectedUnit = "degree";
   let number = 123456.789;
+
+  let setDisplayUnit = () => (selectedFormatter = "unit");
+  let setDisplayCurrency = () => (selectedFormatter = "currency");
 
   let onClick = (options: OptionValues) => {
     copyToClipboard(
@@ -24,43 +33,85 @@
 </script>
 
 <div class="options">
-  <Select
-    name="currencies"
-    placeholder="Select a currency"
-    label="Currency"
-    bind:value={selectedCurrency}
-    items={currenciesAsEntries}
-  />
+  <button on:click={setDisplayUnit}>Unit</button>
+  <button on:click={setDisplayCurrency}>Currency</button>
+  {#if selectedFormatter === "currency"}
+    <Select
+      name="currencies"
+      placeholder="Select a currency"
+      label="Currency"
+      bind:value={selectedCurrency}
+      items={currenciesAsEntries}
+    />
+  {/if}
+  {#if selectedFormatter === "unit"}
+    <Select
+      name="unit"
+      placeholder="Select a unit"
+      label="Unit"
+      bind:value={selectedUnit}
+      items={unitsAsEntries}
+    />
+  {/if}
   <label for="amount">Amount</label>
   <input type="text" bind:value={number} id="amount" />
 </div>
 
 <Grid>
-  {#each [...numberFormatOptionsCurrency] as [option, values]}
-    <OptionSection header={option}>
-      {#each values as value}
-        {#if value !== undefined}
-          <Highlight
-            {onClick}
-            values={{
-              [option]: value,
-              style: "currency",
-              currency: selectedCurrency,
-            }}
-            output={new Intl.NumberFormat(selectedLocale, {
-              style: "currency",
-              currency: selectedCurrency,
-              [option]: value,
-            }).format(number)}
-          />
-        {/if}
-      {/each}
-    </OptionSection>
-  {/each}
+  {#if selectedFormatter === "currency"}
+    {#each [...numberFormatOptionsCurrency] as [option, values]}
+      <OptionSection header={option}>
+        {#each values as value}
+          {#if value !== undefined}
+            <Highlight
+              {onClick}
+              values={{
+                [option]: value,
+                style: "currency",
+                currency: selectedCurrency,
+              }}
+              output={new Intl.NumberFormat(selectedLocale, {
+                style: "currency",
+                currency: selectedCurrency,
+                [option]: value,
+              }).format(number)}
+            />
+          {/if}
+        {/each}
+      </OptionSection>
+    {/each}
+  {/if}
+  {#if selectedFormatter === "unit"}
+    {#each [...numberFormatOptionsUnit] as [option, values]}
+      <OptionSection header={option}>
+        {#each values as value}
+          {#if value !== undefined}
+            <Highlight
+              {onClick}
+              values={{
+                style: "unit",
+                unit: selectedUnit,
+                [option]: value,
+              }}
+              output={new Intl.NumberFormat(selectedLocale, {
+                style: "unit",
+                unit: selectedUnit,
+                [option]: value,
+              }).format(number)}
+            />
+          {/if}
+        {/each}
+      </OptionSection>
+    {/each}
+  {/if}
 </Grid>
 
 <style>
   .options {
     padding-bottom: 1rem;
+  }
+  button {
+    padding: 0.5rem 1rem;
+    margin-bottom: 1rem;
   }
 </style>
