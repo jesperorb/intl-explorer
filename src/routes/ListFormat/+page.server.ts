c6602c5865cb670730e02f5ae25f04e8aa7e18b8
Jesper Orb <github@jesperorb.com>
2022-10-09 08:17:11 +0200

    fix: correct types on page load return

--- src/routes/ListFormat/+page.server.ts
+++ src/routes/ListFormat/+page.server.ts
@@ -1,6 +1,7 @@
-import { loadJson } from '$lib/utils/load-json';
+import type { BrowserCompatData } from '$lib/types/BrowserSupport.types';
 import type { ServerLoadEvent } from '@sveltejs/kit';
+import { loadJson } from '$lib/utils/load-json';
 
-export async function load(loadEvent: ServerLoadEvent) {
+export async function load(loadEvent: ServerLoadEvent): Promise<BrowserCompatData> {
 	return loadJson(loadEvent);
 }
