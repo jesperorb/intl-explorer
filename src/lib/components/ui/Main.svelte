5f7020c88582df177cb79bc93041e0f8a2269e1f
Jesper Orb <github@jesperorb.com>
2024-03-31 10:19:59 +0200

    fix: front page

--- src/lib/components/ui/Main.svelte
+++ src/lib/components/ui/Main.svelte
@@ -1,26 +1,23 @@
-<main id="main" tabindex="-1">
+<script lang="ts">
+	export let center: boolean | undefined = undefined;
+</script>
+
+<main id="main" class:center={center} tabindex="-1">
 	<div class="wrapper">
 		<slot />
 	</div>
 </main>
 
 <style>
-	main {
-		flex: 1;
-	}
-	@media (min-width: 900px) {
-		main {
-			z-index: 2;
-			max-width: 100%;
-		}
-		.wrapper {
-			border-radius: 6px 0 0 6px;
-		}
+	.center {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 	.wrapper {
+		border-radius: 4px;
 		background-color: var(--background-color);
 		border: 1px solid var(--border-color);
-		border-right: 0;
 		padding: var(--spacing-4);
 	}
 </style>
